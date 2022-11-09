36b1d44cf6a44addbe5a871407904292bbe9da12
Hephezy <oluwapelumimartins5@gmail.com>
2022-11-09 05:50:20 +0100

    Corrections

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,8 +15,8 @@ const Navbar = () => {
         <div className="gpt3__navbar-links_container">
           <p><a href="#home">Home</a></p>
           <p><a href="#wgpt3">What is GPT3?</a></p>
-          <p><a href="#possibility">Open AI</a></p>
-          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#open-ai">Open AI</a></p>
+          <p><a href="#case-studies">Case Studies</a></p>
           <p><a href="#blog">Library</a></p>
         </div>
       </div>
--- src/containers/index.js
+++ src/containers/index.js
@@ -3,4 +3,4 @@ export { default as Features } from './Features/Features';
 export { default as Footer } from './Footer/Footer';
 export { default as Header } from './Header/Header';
 export { default as Possibility } from './Possibility/Possibility';
-export { default as WhatGPT3 } from './WhatGpt3/WhatGpt3';
+export { default as WhatGpt3 } from './WhatGpt3/WhatGpt3';
